bfabbcaa1eaa201b1330a4eae99612ddfeedd0d2
Nikita <325vi7001@gmail.com>
2024-11-01 09:46:51 +0300

    lab1. Создание каркаса приложения

---
+++ ToursApp/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using ToursApp.Pages;
+namespace ToursApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new HotelsPage());
+            Manager.MainFrame = MainFrame;
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.GoBack();
+        }
+
+        private void MainFrame_ContentRendered(object sender, System.EventArgs e)
+        {
+            if (MainFrame.CanGoBack) { 
+                BtnBack.Visibility = Visibility.Visible;
+            }
+            else { 
+                BtnBack.Visibility= Visibility.Hidden;
+            }
+        }
+    }
+}
